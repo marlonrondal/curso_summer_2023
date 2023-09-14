56f09ec43dc1b160be13fe3b68ccefbfbeb1fa70
marlonrondal <marlon.benavides23@gmail.com>
2023-09-14 08:39:29 +0200

    Entidades hechas

---
+++ API/Entidades/Personas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public class Personas
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string nombre { get; set; }
+
+        [Required]
+        public DateTime fechaNacimiento { get; set; }
+
+        [MaxLength(25)]
+        public string telefono { get; set; }
+    }
+}
