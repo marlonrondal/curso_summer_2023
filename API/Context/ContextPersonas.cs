4d65f11ab5f42b9e21a6a1c8c98a2115ccbb290d
marlonrondal <marlon.benavides23@gmail.com>
2023-09-14 08:47:19 +0200

    Context hecho

---
+++ API/Context/ContextPersonas.cs
@@ -0,0 +1,15 @@
+﻿using Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Context
+{
+    public class ContextPersonas : DbContext
+    {
+        public ContextPersonas(DbContextOptions<ContextPersonas> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Personas> personas { get; set; }
+    }
+}
