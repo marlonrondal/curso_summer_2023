bad7e283b06f9061c6a3cc8e7f50e3aa2736f911
marlonrondal <marlon.benavides23@gmail.com>
2023-09-14 08:43:07 +0200

    EntodadesDTO hecha

---
+++ API/EntidadesDTO/CrearPersonas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDTO
+{
+    public class CrearPersonas
+    {
+        public string nombre { get; set; }
+
+        public DateTime fechaNacimiento { get; set; }
+
+        public string telefono { get; set; }
+    }
+}
---
+++ API/EntidadesDTO/PersonasVer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDTO
+{
+    public class PersonasVer
+    {
+        public Guid id { get; set; }
+        public string nombre { get; set; }
+
+        public DateTime fechaNacimiento { get; set; }
+
+        public string telefono { get; set; }
+    }
+}
