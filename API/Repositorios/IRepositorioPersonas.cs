a1233074bec33176ff44ecae8cad217d102fea38
marlonrondal <marlon.benavides23@gmail.com>
2023-09-14 08:57:05 +0200

    repositorios hechos

---
+++ API/Repositorios/IRepositorioPersonas.cs
@@ -0,0 +1,12 @@
+﻿using Entidades;
+
+namespace Repositorios
+{
+    public interface IRepositorioPersonas
+    {
+        bool exitePersona(Guid id);
+        Task<Personas> altaPersona(Personas nuevaPersona);
+        Task<Personas> obtenerPersona(Guid id);
+        Task<IEnumerable<Personas>> obtenerPrimeras10();
+    }
+}
---
+++ API/Repositorios/RepositorioPersonas.cs
@@ -0,0 +1,76 @@
+﻿using Entidades;
+using Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositorios
+{
+    public class RepositorioPersonas : IRepositorioPersonas
+    {
+
+        private readonly ContextPersonas _context;
+
+        public RepositorioPersonas(ContextPersonas _context)
+        {
+            this._context = _context;
+        }
+
+        // Existe persona
+        public bool exitePersona(Guid id)
+        {
+            Personas personas = _context.personas.FirstOrDefault(p => p.id == id);
+            return personas != null ? true : false;
+        }
+
+        // Alta persona
+        public async Task<Personas> altaPersona(Personas nuevaPersona)
+        {
+            if (exitePersona(nuevaPersona.id))
+            {
+                Personas existePersona = _context.personas.First(p => p.id == nuevaPersona.id);
+                existePersona.nombre = nuevaPersona.nombre;
+                existePersona.fechaNacimiento = nuevaPersona.fechaNacimiento;
+                existePersona.telefono = nuevaPersona.telefono;
+            }
+            else
+            {
+                _context.personas.Add(nuevaPersona);
+            }
+
+            _context.SaveChanges();
+            return nuevaPersona;
+        }
+
+        // Obtener Una Persona
+        public async Task<Personas> obtenerPersona(Guid id)
+        {
+
+            return _context.personas.FirstOrDefault(p => p.id == id);
+        }
+
+        // Primeras 10 personas con más de 21 años ordenado por nombre
+        public async Task<IEnumerable<Personas>> obtenerPrimeras10()
+        {
+            var personasMayoresDe21 = await _context.personas.ToListAsync();
+
+            var ultimosMayoresDe21 = personasMayoresDe21
+                .Where(p => calcularEdad(p.fechaNacimiento) > 21)
+                .OrderBy(p => p.nombre)
+                .TakeLast(10)
+                .ToList();
+
+            return ultimosMayoresDe21;
+        }
+
+
+
+        private int calcularEdad(DateTime fechaNacimiento)
+        {
+            DateTime fechaActual = DateTime.Now;
+
+            TimeSpan diferenciaFechas = fechaActual - fechaNacimiento;
+
+            return diferenciaFechas.Days / 365;
+        }
+
+    }
+}
